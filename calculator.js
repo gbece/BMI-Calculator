cd1f9d3a529a0bb3c220fc9654d7de7760ce2bd0
Gonzalo Becerra <70300584+gbece@users.noreply.github.com>
2022-11-28 10:54:13 -0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,40 @@
+const express = require ('express');
+const bodyParser = require ("body-parser");
+
+const app= express();
+app.use(bodyParser.urlencoded({extended: true})); // Sirve para pasar los parametros del http request
+
+app.get('/', function(req, res){
+
+  res.sendFile(__dirname+"/index.html");
+  //console.log(__dirname);
+});
+
+app.post('/', function (req, res){
+  var num1=Number(req.body.num1);
+  var num2=Number(req.body.num2);
+
+  var result= num1+num2;
+  res.send("The result of the calculation is: "+result);
+});
+
+app.get('/bmicalculator', function(req, res){
+
+  res.sendFile(__dirname+"/bmiCalculator.html");
+  //console.log(__dirname);
+});
+
+app.post('/bmicalculator', function (req, res){
+  var weight=Number(req.body.weight);
+  var height=Number(req.body.height);
+
+  var bmi=(weight/(height*height));
+
+  res.send("Your BMI is : "+bmi);
+});
+
+
+
+app.listen(3000,function(){
+  console.log("Server conected to port 3000");
+});
